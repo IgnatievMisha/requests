772e54f58a52006ad85edf5d6fde4abe78bd212b
Студент <student@ITStep.local>
2023-05-25 19:38:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import urllib.request
+opener=urllib.request.build_opener()
+response=opener.open("https://httpbin.org/get")
+print(response.read())
+
+import requests
+response=requests.get("https://coinmarketcap.com/")
+
+
+'''print(response.content)
+response_parse=response.text.split('<span>')
+for elem in response_parse:
+    if elem.startswith(("$")):
+        print(elem)'''
+
+
+from bs4 import BeautifulSoup
+if response.status_code==200:
+    soup=BeautifulSoup(response.text, features="html.parser")
+    soup_list=soup.find_all('a', {'href': "/currencies/bitcoin/markets/"})
+    res=soup_list[0].find('span')
+    print(res.text)
